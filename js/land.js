7afc954ae97ad92e3d55d118a2e63b7e9346b876
canontank <58597814+canontank@users.noreply.github.com>
2022-04-01 15:39:01 +0900

    Add files via upload

--- js/land.js
+++ js/land.js
@@ -1,19 +1,80 @@
+var rows;
+
+var landType = "";
+var areaType = "";
+
 $(document).ready(function() {
 	$.ajax({
 		type: "GET",
-        //url : "https://script.google.com/macros/s/AKfycbyU8GSUExQma8iaXt4mhA_Z7yG1U-mKxhLC8d_89pFO_nbyrthN0bn5VZfzy3ASF7r_/exec",
         url : "https://script.google.com/macros/s/AKfycbwXig5n3LymBlnmLOm_7oEd240oILHTGbg8GtA39i2FR8tMRCsegrQc2OvEzGtWLAjH6Q/exec",
 		data: {
 			"cmd" : "get"
 		},
-        success : function(rows) {
-			setLandTitle();
-			setLandContents(rows);
-		}
+        success : function(result) {
+            rows = result;
+            rows.reverse();
+            execute1();
+ 		}
 	});
+    setBind();
 });
 
+function setBind() {
+    $("#landType").on("change", function() {
+        landType = $(this).val();
+        execute2();
+    });
+    
+    $("#areaType").on("change", function() {
+        areaType = $(this).val();
+        execute3();
+    });
+}
+
+function execute1() {
+    setLandType();
+    execute2();
+}
+
+function execute2() {
+    setAreaType();
+    execute3();
+}
+
+function execute3() {
+    setLandTitle();
+    setLandContents();
+}
+
+function setLandType() {
+    var typeSet = new Set();
+    for (var row of rows) {
+        typeSet.add(row["단지명"]);
+    }
+    for (var type of typeSet) {
+        $('#landType').append($('<option/>', { value : type, text : type }));
+    }
+    var iter = typeSet.values();
+    landType = iter.next().value;
+}
+
+function setAreaType() {
+    var areaSet = new Set();
+    for (var row of rows) {
+        if (landType != row["단지명"])
+            continue;
+        areaSet.add(row["전용면적"]);
+    }
+    var areaArray = Array.from(areaSet);
+    areaArray.sort();
+    $('#areaType').append($('<option/>', { value : "", text : "전체" }));
+    for (var area of areaArray) {
+        $('#areaType').append($('<option/>', { value : area, text : area }));
+    }
+}
+
 function setLandTitle() {
+    $('#land tr').remove();
 	$("#land").append($('<tr/>')
 		.append($('<th/>', { html : '날짜' }))
 		.append($('<th/>', { html : '면적(㎡)' }))
@@ -22,13 +83,16 @@ function setLandTitle() {
 	);
 }
 
-function setLandContents(rows) {
-	rows.reverse();
+function setLandContents() {
 	for (var row of rows) {
+        if (landType != row["단지명"])
+            continue;
+        if (areaType != "" && areaType != row["전용면적"])
+            continue;
 		$("#land").append($('<tr/>', {})
 			.append($('<td/>', { html : row['날짜'], align : 'center' }))
-			.append($('<td/>', { html : row['전용면적'] }))
-			.append($('<td/>', { html : row['층'] }))
+			.append($('<td/>', { html : row['전용면적'], align : 'center'  }))
+			.append($('<td/>', { html : row['층'], align : 'center'  }))
 			.append($('<td/>', { html : row['거래금액'] }))
 		);
 	}
